15c91b8cc279b03823c5a1f9e793210949e59ce8
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-05-31 20:57:36 +0100

    Update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,22 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
+   async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ];
+  },
   mount: {
     public: '/',
     src: '/_dist_',
