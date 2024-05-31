c33320def03327fcf59ae79b634bdb36ae6cf5c3
FantasyZoom22 <93161122+FantasyZoom22@users.noreply.github.com>
2024-05-31 19:08:48 +0100

    Update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     public: '/',
     src: '/_dist_',
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: ["@babel/plugin-transform-react-jsx", { "plugins": ["react-refresh/babel", { "skipEnvCheck": true }] }, '@snowpack/plugin-dotenv'],
   install: [
     /* ... */
   ],
